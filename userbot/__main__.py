a9bde4dd295813eca84b44a13223d81b76003f1a
OMNIPOT <79632234+OMNIPOT@users.noreply.github.com>
2021-03-11 16:18:36 +0000

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"Lord-Userbot ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
+    f"Flame-USbot 🔥 V{BOT_VER} [TELAH BERKOBAR!]")
 
 
 if len(argv) not in (1, 3, 4):
