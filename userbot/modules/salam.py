dd02f3e1999de2140cb1673d332a72f34ec4a70b
OMNIPOT <79632234+OMNIPOT@users.noreply.github.com>
2021-03-11 18:22:04 +0000

    Update salam.py

--- userbot/modules/salam.py
+++ userbot/modules/salam.py
@@ -10,7 +10,7 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 @register(outgoing=True, pattern='^.P(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("Assalamu'alaikum Kawand")
+    await typew.edit("Assalamu'alaikum Warahmatullahi Wabarakatuh")
 
 
 @register(outgoing=True, pattern='^.p(?: |$)(.*)')
@@ -22,7 +22,7 @@ async def typewriter(typew):
 @register(outgoing=True, pattern='^.L(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("Wa'alaikumussalam Kawand")
+    await typew.edit("Wa'alaikumussalam Warahmatullahi Wabarakatuh")
 
 
 @register(outgoing=True, pattern='^.l(?: |$)(.*)')
