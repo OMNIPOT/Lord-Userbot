f7071bf2e7070db42af32d5ef11e40f8ee714314
OMNIPOT <79632234+OMNIPOT@users.noreply.github.com>
2021-03-12 16:27:31 +0000

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -65,23 +65,23 @@ async def sleepybot(time):
 @register(outgoing=True, pattern="^.shutdown$")
 async def killdabot(event):
     """ For .shutdown command, shut the bot down."""
-    await event.edit("`Mematikan Flame-USbot....`")
+    await event.edit("`Mematikan Pakdhe-Usbot....`")
     await asyncio.sleep(7)
     await event.delete()
     if BOTLOG:
         await event.client.send_message(BOTLOG_CHATID, "#SHUTDOWN \n"
-                                        "`Flame-USbot Telah Dimatikan`")
+                                        "`Pakdhe-Usbot Telah Dimatikan`")
     await bot.disconnect()
 
 
 @register(outgoing=True, pattern="^.restart$")
 async def killdabot(event):
-    await event.edit("`Mengulang Kembali Flame-USbot...`")
+    await event.edit("`Mengulang Kembali Pakdhe-Usbot...`")
     await asyncio.sleep(10)
     await event.delete()
     if BOTLOG:
         await event.client.send_message(BOTLOG_CHATID, "#RESTARTBOT \n"
-                                        "`Flame-USbot Telah Di Restart`")
+                                        "`Pakdhe-Usbot Telah Di Restart`")
     await bot.disconnect()
     # Spin a new instance of bot
     execl(sys.executable, sys.executable, *sys.argv)
@@ -93,7 +93,7 @@ async def killdabot(event):
 async def reedme(e):
     await e.edit(
         "Here's something for you to read:\n"
-        "\n[Flame-USbot Repo](https://github.com/OMNIPOT/Lord-Userbot)"
+        "\n[Pakdhe-Usbot Repo](https://github.com/OMNIPOT/Lord-Userbot)"
         "\n[Setup Guide - Basic](https://telegra.ph/How-to-host-a-Telegram-Userbot-11-02)"
         "\n[Setup Guide - Google Drive](https://telegra.ph/How-To-Setup-GDrive-11-02)"
         "\n[Setup Guide - LastFM Module](https://telegra.ph/How-to-set-up-LastFM-module-for-Paperplane-userbot-11-02)"
@@ -120,7 +120,7 @@ async def repeat(rep):
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
     await wannasee.edit(
-        "🔥 **Repo Userbot:** [Flame-USbot](https://github.com/OMNIPOT/Lord-Userbot)\n🔥 **Pengubah:** [Ardian](t.me/Establishedd)"
+        "🔥 **Repo Userbot:** [Pakdhe-Usbot](https://github.com/OMNIPOT/Lord-Userbot)\n🔥 **Pengubah:** [Ardian](t.me/Establishedd)"
     )
 
 
